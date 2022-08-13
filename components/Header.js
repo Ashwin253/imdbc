055f7040e3f447cca98d488ed04aebabcdba39f2
Ashwin253 <ashwingeorge25@gmail.com>
2022-08-13 15:34:11 +0530

    header component

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from 'react';
+import Image from 'next/image';
+import { HomeIcon,UserIcon,PhoneIcon,InformationCircleIcon } from '@heroicons/react/solid';
+import Headericon from './headericon';
 export default function Header() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="">
+    <Headericon Icon={HomeIcon} title="HOME"/>
+    <Headericon Icon={UserIcon} title="ACCOUNT"/>
+    <Headericon Icon={PhoneIcon} title="CONTACT"/>
+    <Headericon Icon={InformationCircleIcon} title="ABOUT"/>
+    </div>
+   <div className=''>
+    <Image src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" width={100} height={100}/>
+   </div>
     </>
   )
 }
---
+++ components/headericon.js
@@ -0,0 +1,8 @@
+export default function Headericon({Icon,title}) {
+  return (
+    <div>
+        <Icon className="h-8"/>
+        <p>{title}</p>
+    </div>
+  )
+}
