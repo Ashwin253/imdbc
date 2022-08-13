a43889eb55289f22d4999c9837e1f6c88fe6c14b
Ashwin253 <ashwingeorge25@gmail.com>
2022-08-13 15:49:44 +0530

    header styling

--- components/Header.js
+++ components/Header.js
@@ -4,16 +4,16 @@ import { HomeIcon,UserIcon,PhoneIcon,InformationCircleIcon } from '@heroicons/re
 import Headericon from './headericon';
 export default function Header() {
   return (
-    <>
-    <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between" >
+    <div className="flex">
     <Headericon Icon={HomeIcon} title="HOME"/>
     <Headericon Icon={UserIcon} title="ACCOUNT"/>
     <Headericon Icon={PhoneIcon} title="CONTACT"/>
     <Headericon Icon={InformationCircleIcon} title="ABOUT"/>
     </div>
-   <div className=''>
-    <Image src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" width={100} height={100}/>
+   <div>
+    <Image src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" width={100} height={100} className="cursor-pointer active:brightness-110"/>
    </div>
-    </>
+    </div>
   )
 }
